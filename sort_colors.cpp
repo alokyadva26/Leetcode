4d8b88bd0e3b203e97796cfbf82e8667740e0ecb
Alok Yadav <24bcs10744@gmail.com>
2026-04-23 01:43:23 +0530

    solve sort colors with optimal solution - Dutch National Flag algorithm

--- sort_colors.cpp
+++ sort_colors.cpp
@@ -3,13 +3,36 @@
 #include <algorithm>
 using namespace std;
 
+// using sort function but the main question is to not use sort function
+// vector<int> sortcolors(vector<int>& arr){
+//     sort(arr.begin(), arr.end());
+//     vector<int> result = arr;
+//     return arr;
+    
+// }
+
+//Approach without using sort function: 
 vector<int> sortcolors(vector<int>& arr){
-    sort(arr.begin(), arr.end());
-    vector<int> result = arr;
-    return arr;
+    int low = 0 , mid = 0 , high = arr.size()-1;
+
+    while(mid<= high){
+        if(arr[mid]>= arr[high]){
+            swap(arr[mid], arr[high]);
+            high--;
+        }else if(arr[mid]==0){
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }else{
+            mid++;
+        }
+    }
+    return vector<int> (arr);
     
 }
-// using sort function but the main question is to not use sort function
+
+
+
 int main(){
     vector<int> arr = {2,0,2,1,1,0};
     vector<int> sorted_arr = sortcolors(arr);
@@ -20,3 +43,5 @@ int main(){
     }
     return 0;
 }
+
+
