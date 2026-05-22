aabc0cf7006288f31d61c4d48d2f01e776f0ac56
Alok Yadav <24bcs10744@gmail.com>
2026-05-22 22:25:53 +0530

    optimize the valid palindorme problem

---
+++ String/valid2_palindrome.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+bool isPalindrome(string s){
+    int i = 0 , j = s.size() - 1;
+    while( i < j && !isalnum(s[i])) i++;
+    while(i < j && !isalnum(s[j])) j--;
+
+    if(tolower(s[i])!= tolower(s[j])) {
+        return false;
+    }
+    i++;
+    j--;
+    return true;
+}
+int main(){
+    string s = "A man, a plan, a canal: Panama";
+    bool result = isPalindrome(s);
+    cout<< result <<endl;
+}
