7c39c96a5ce62bd492d770abff8b4663c0cd30e4
Alok Yadav <24bcs10744@gmail.com>
2026-05-24 00:56:54 +0530

    check the given array is sorted or not

---
+++ Array/check_sort_or_not.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+bool isSorted(int arr[] ,  int n){
+    for(int i = 1 ; i < n ; i++){
+        if(arr[i] < arr[i-1]) return false;
+    }
+    return true;
+}
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    if(isSorted(arr , n)){
+        cout << "The array is sorted." << endl;
+    }
+    else{
+        cout << "The array is not sorted." << endl;
+    }
+    return 0;
+}
