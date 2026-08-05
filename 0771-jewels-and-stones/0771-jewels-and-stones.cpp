6ba33cd96dec619bf7228a671ed428658c73cf25
Alok Yadav <24bcs10744@gmail.com>
2026-08-05 21:37:36 +0530

    Time: 0 ms (100%), Space: 8.6 MB (43.1%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> st;
+
+        for(char ch : jewels){
+            st.insert(ch);
+        }
+
+        int count = 0;
+
+        for(char ch : stones){
+            if(st.count(ch)){
+                count++;
+            }
+        }
+        return count;
+    }
+};
